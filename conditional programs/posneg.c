ca106aa392c45850423e47bff9fb78f06aebd76a
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-02-23 15:13:38 +0530

    Add files via upload

---
+++ conditional programs/posneg.c
@@ -0,0 +1,18 @@
+// check whether a number is positive or negative
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number :");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("Number is Positive!!");
+    }
+    else
+    {
+        printf("Number is Negative!!");
+    }
+    return 0;
+}
