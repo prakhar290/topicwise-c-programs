891fa8c47c6655a4792d013dd7f0c4cce8c70700
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-06-24 22:38:23 +0530

    Add files via upload

---
+++ array problems/insertion-sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main ()
+{
+int n,i,j, k,temp;
+printf("Enter the number of elements : ");
+scanf("%d",&n);
+int a[n];
+printf("\nEnter elements of array : ");
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+printf("\nArray is :\n");
+for(i=0;i<n;i++)
+{
+    printf(" %d",a[i]);
+}
+for(i=1;i<n;i++)
+{
+    int current = a[i];
+    j = i-1;
+    while(a[j]>current && j>=0)
+    {
+        a[j+1] = a[j];
+        j--;
+    }
+    a[j+1] = current;
+}
+printf("\nSorted Array is :\n");
+for(i=0;i<n;i++)
+{
+    printf(" %d",a[i]);
+}
+return 0;
+}
